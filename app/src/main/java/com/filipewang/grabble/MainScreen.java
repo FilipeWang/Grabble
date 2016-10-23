29d86c83d6898703399de6ed789890af0b7f5ad3
Filipe Wang <filipe.wang@gmail.com>
2016-10-23 15:34:02 +0100

    Added a settings screen with dummy options.

--- app/src/main/java/com/filipewang/grabble/MainScreen.java
+++ app/src/main/java/com/filipewang/grabble/MainScreen.java
@@ -1,7 +1,10 @@
 package com.filipewang.grabble;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        ImageButton settingsButton = (ImageButton) findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainScreen.this,SettingsScreen.class));
+            }
+        });
     }
 }
